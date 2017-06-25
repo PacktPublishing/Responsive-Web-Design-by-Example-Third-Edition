77baeec81b6d5928154a79da3b80de88bd6c8f55
Sonar Systems <SonarSystems@users.noreply.github.com>
2017-06-25 22:01:10 +0100

    Chapter 4 Code 10 Commit

---
+++ Chapter 4/Code 10/JS/index.js
@@ -0,0 +1,13 @@
+$( function( )
+{
+    $( document ).on( 'click', '.navButton', function( event )
+    {
+ 		$( 'html, body' ).animate(
+ 		{
+ 			scrollTop: $( $( this ).attr('href') ).offset( ).top
+       	}, 2000 );
+
+       
+        event.preventDefault( );
+    } );
+} );
