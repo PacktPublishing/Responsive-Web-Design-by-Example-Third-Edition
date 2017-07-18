dbc1d0191c09b7bf7b7f027c3bde4b180f062ac5
Sonar Systems <SonarSystems@users.noreply.github.com>
2017-07-18 19:02:07 +0100

    Chapter 8 Code 1 - 3 Commit

---
+++ Chapter 8/Code 1/post.php
@@ -0,0 +1,4 @@
+<?php require_once( "SNIPPETS/HEADER.php" ); ?>
+
+
+<?php require_once( "SNIPPETS/FOOTER.php" ); ?>
---
+++ Chapter 8/Code 2/post.php
@@ -0,0 +1,13 @@
+<?php require_once( "SNIPPETS/HEADER.php" ); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-9">
+      <h1>Half-Life 3</h1>
+
+      <img src="https://venturebeat.com/wp-content/uploads/2013/03/hl2ep2postercropped.jpg?strip=all" />
+    </div>
+  </div>
+</div>
+
+<?php require_once( "SNIPPETS/FOOTER.php" ); ?>
