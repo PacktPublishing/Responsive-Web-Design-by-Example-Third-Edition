b7fe0f430ce2d1d40b6c12ab54960f0678c7332a
Sonar Systems <SonarSystems@users.noreply.github.com>
2017-08-13 17:45:20 +0100

    Chapter 11 Code 7 Commit

---
+++ Chapter 11/Code 7/profile.php
@@ -0,0 +1,112 @@
+<?php require_once( "SNIPPETS/HEADER.php" ); ?>
+
+<div id="ProfileJumbotron" class="jumbotron jumbotron-fluid">
+	<div class="row">
+		<div class="col-12">
+    		<img id="ProfileBannerImage" src="https://lh3.googleusercontent.com/-ShX0GMbqgO0/VH8qDnLhurI/AAAAAAAAAas/sJvfY7eWOp0ZI9YFffcOhwRtPgDGc_JhgCL0BGAYYCw/s1000-fcrop64=1,01ab0000ffa9ffff/Youtube%2Bnew%2Bheader.png" />
+
+    		<div class="carousel-caption">
+				<h3>
+					Sonar Systems
+				</h3>
+
+				<p>
+					1,000,000,000 Follows :D
+				</p>
+			</div>
+    	</div>
+	</div>
+</div>
+
+<div class="row">
+    <div class="col-lg-3 col-6 cardContainer">
+      <div class="card">
+        <a href="post.php"><img class="card-img-top cardImage" src="https://lh3.googleusercontent.com/-t7s6QT-8e30/AAAAAAAAAAI/AAAAAAAAAAA/sX5duDFyQ5s/w320-h180-p-rw/photo.jpg" alt="Card image cap"></a>
+
+        <div class="card-block">
+          <a href="post.php"><h4 class="card-title">Interest 1</h4></a>
+
+          <p class="card-text">1000 Members</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-6 cardContainer">
+      <div class="card">
+        <a href="post.php"><img class="card-img-top cardImage" src="https://lh3.googleusercontent.com/-t7s6QT-8e30/AAAAAAAAAAI/AAAAAAAAAAA/sX5duDFyQ5s/w320-h180-p-rw/photo.jpg" alt="Card image cap"></a>
+
+        <div class="card-block">
+          <a href="post.php"><h4 class="card-title">Interest 2</h4></a>
+
+          <p class="card-text">1000 Members</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-6 cardContainer">
+      <div class="card">
+        <a href="post.php"><img class="card-img-top cardImage" src="https://lh3.googleusercontent.com/-t7s6QT-8e30/AAAAAAAAAAI/AAAAAAAAAAA/sX5duDFyQ5s/w320-h180-p-rw/photo.jpg" alt="Card image cap"></a>
+
+        <div class="card-block">
+          <a href="post.php"><h4 class="card-title">Interest 3</h4></a>
+
+          <p class="card-text">1000 Members</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-6 cardContainer">
+      <div class="card">
+        <a href="post.php"><img class="card-img-top cardImage" src="https://lh3.googleusercontent.com/-t7s6QT-8e30/AAAAAAAAAAI/AAAAAAAAAAA/sX5duDFyQ5s/w320-h180-p-rw/photo.jpg" alt="Card image cap"></a>
+
+        <div class="card-block">
+          <a href="post.php"><h4 class="card-title">Interest 4</h4></a>
+
+          <p class="card-text">1000 Members</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="row">
+	<div class="col-lg-6 col-12 postContainer">
+		<div class="card">
+		  	<div class="card-block">
+			    <div class="media">
+			  		<img class="d-flex mr-3 postThumbnail" src="https://t5.rbxcdn.com/e96e5ea34a7af7ac1083f9c0d81de638">
+
+			  		<div class="media-body">
+    					<h5 class="mt-0">Frahaan Hussain</h5>
+    					<i>1st January 2000</i>
+
+    					<div class="postMainBody">
+    						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    					</div>
+  					</div>
+				</div>
+		  	</div>
+		</div>
+	</div>
+
+	<div class="col-lg-6 col-12 postContainer">
+		<div class="card">
+		  	<div class="card-block">
+			    <div class="media">
+			  		<img class="d-flex mr-3 postThumbnail" src="https://t5.rbxcdn.com/e96e5ea34a7af7ac1083f9c0d81de638">
+
+			  		<div class="media-body">
+    					<h5 class="mt-0">Frahaan Hussain</h5>
+    					<i>1st January 2000</i>
+
+    					<div class="postMainBody">
+    						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    					</div>
+  					</div>
+				</div>
+		  	</div>
+		</div>
+	</div>
+</div>
+
+
+<?php require_once( "SNIPPETS/FOOTER.php" ); ?>
