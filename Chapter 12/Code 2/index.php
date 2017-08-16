6697295dc7e3cdf7e977333a2a2ed9c71adb0fc8
Sonar Systems <SonarSystems@users.noreply.github.com>
2017-08-16 17:47:17 +0100

    Chapter 12 Code 1 and 2 Commit

---
+++ Chapter 12/Code 2/index.php
@@ -0,0 +1,11 @@
+<?php require_once( "SNIPPETS/HEADER.php" ); ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-12 text-center">
+			<h1>The Best Photo Gallery In The World</h1>
+		</div>
+	</div>
+</div>
+
+<?php require_once( "SNIPPETS/FOOTER.php" ); ?>
